a8d8f7fc09d15f68967ea9c5185b9a985400beea
mvd366 <michaeld366@yahoo.com>
2013-09-27 08:12:15 -0400

    Create background SMS Monitor Service and enable toggle switch in layout.

--- src/com/mikedilalo/autoreply/MainActivity.java
+++ src/com/mikedilalo/autoreply/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends Activity {
@@ -14,6 +17,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+	    
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
@@ -21,6 +25,27 @@ public class MainActivity extends Activity {
         AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
         am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
         Log.i(TAG,"Silent mode");
+    }
+    
+    public void onMainToggleClicked(View view) {
+    	
+    	// Is AutoReply enabled?
+    	boolean on = ((ToggleButton) view).isChecked();
+    	
+    	if (on) {
+    		// Creates SMSMonitorService intent
+    	    Intent SMSMonitorIntent = new Intent(this, SMSMonitorService.class);
+    		// Start SMSMonitorService
+    		startService(SMSMonitorIntent);
+    		Log.i(TAG,"SMSMonitor Started");
+    		
+    	} else {
+    		// Creates SMSMonitorService intent
+    	    Intent SMSMonitorIntent = new Intent(this, SMSMonitorService.class);
+    		// Start SMSMonitorService
+    		stopService(SMSMonitorIntent);
+    		Log.i(TAG,"SMSMonitor Stopped");
+    	}
     }
 
 
